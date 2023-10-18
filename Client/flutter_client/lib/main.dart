86a07236bdcc915974f1db53fd68cd6f986a71d4
anirudh13verma <anirudh13verma@gmail.com>
2023-10-18 21:39:10 +0530

    relocate

--- Client/flutter_client/lib/main.dart
+++ Client/flutter_client/lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(body: StartPage()),
     );
